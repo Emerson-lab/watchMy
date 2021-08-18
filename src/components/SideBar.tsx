cfcf1b88d34748c6261bdef0607094553242e49d
Emerson <suporte@portasabertas.org.br>
2021-08-18 18:37:27 -0300

    first commit

--- src/components/SideBar.tsx
+++ src/components/SideBar.tsx
@@ -1,3 +1,20 @@
-export function SideBar() {
-  // Complete aqui
+import { Icon } from './Icon';
+
+import '../styles/button.scss';
+import { ButtonHTMLAttributes } from 'react';
+
+interface ButtonProps extends ButtonHTMLAttributes<HTMLButtonElement> {
+  title: string;
+  iconName: 'action' | 'comedy' | 'documentary' | 'drama' | 'horror' | 'family';
+  selected?: boolean;
+}
+
+export function SideBar({ iconName, title, selected, ...rest }: ButtonProps) {
+  return (
+    
+    <button type="button" {...(selected && { className: 'selected' })} {...rest}>
+      <Icon name={iconName} color={selected ? '#FAE800' : '#FBFBFB'} />
+      {title}
+    </button>
+  );
 }
